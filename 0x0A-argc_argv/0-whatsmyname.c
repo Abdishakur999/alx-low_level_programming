397ca05fb42df288b3405c485d9b340af591a45d
Abdishakur <abdishakurabdullahi9@gmail.com>
2022-10-05 19:46:40 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ * @argc:number of things entered into command line
+ * @argv:string value of things entered in command line
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+return (0);
+
+}
